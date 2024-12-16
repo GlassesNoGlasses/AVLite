973638dd4b31e6e8e94cb0813c7760e0bac46ffa
Neil Wang <neilywang3@gmail.com>
2024-12-15 20:12:00 -0500

    started refactor

--- monitoring/file_monitor.py
+++ monitoring/file_monitor.py
@@ -1,5 +1,7 @@
 import time
 import logging
+import threading
+import os
 import watchdog
 
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
@@ -7,13 +9,6 @@ from watchdog.observers import Observer
 
 
 class FileEventHandler(FileSystemEventHandler):
-    def __init__(self, paths: list[str]) -> None:
-        if not paths:
-            raise ValueError("No directory paths provided")
-        
-        self.paths = paths
-
-
     def on_created(self, event: FileSystemEvent) -> None:
         logging.info(f"File created: {event.src_path}")
 
@@ -26,6 +21,55 @@ class FileEventHandler(FileSystemEventHandler):
     def on_any_event(self, event: FileSystemEvent) -> None:
         print(event)
 
+class FileMonitor:
+    def __init__(self, paths: list[str]) -> None:
+        if not paths:
+            raise ValueError("File paths must be provided")
+
+        for path in paths:
+            if not os.path.exists(path):
+                raise FileNotFoundError(f"Path {path} does not exist")
+        
+        self.paths = paths
+        self.thread_id = 0 # original thread
+        self.observers = {} # {thead_id: thread(observer)}
+        self.event_handler = FileEventHandler() # event handler for file monitoring
+    
+
+    def start_monitor(self, path: str):
+        ''' Start monitoring the given path '''
+        observer = Observer()
+        observer.schedule(self.event_handler, path, recursive=True)
+        observer.start()
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+            logging.info(f"[{threading.get_ident()}] Stopped monitoring {path}")
+        observer.join()
+
+        
+    def run(self):
+        ''' Start monitoring all paths '''
+
+        for path in self.paths:
+            logging.info(f"[Event] Attempting to monitor {path}...")
+
+            thread = threading.Thread(target=self.start_monitor, args=(path,))
+            thread.start()
+            self.observers[thread.ident] = thread
+
+            logging.info(f"[EVENT] Created thread ${thread.ident}; Monitoring {path}...")
+        
+        logging.info(f"[EVENT] Monitoring started for {len(self.paths)} paths")
+
+
+    def stop_monitor(self, thread_id: int):
+        pass
+
+
 
 if __name__ == "__main__":
     import constants
@@ -34,13 +78,3 @@ if __name__ == "__main__":
                         format='%(asctime)s - %(message)s', 
                         datefmt='%Y-%m-%d %H:%M:%S')
     
-    event_handler = FileEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, ".", recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    finally:
-        observer.stop()
-        observer.join()
